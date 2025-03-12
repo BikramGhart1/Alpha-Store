732227f2b1e8c953c25514f93094deb720ca7392
BikramGhart1 <coludingrain@gmail.com>
2025-03-12 17:16:26 +0545

    add line

--- src/components/ShopByBrands.jsx
+++ src/components/ShopByBrands.jsx
@@ -6,6 +6,7 @@ export default function ShopByBrands() {
     <section className='section flex flex-col '>
       <div className='flex flex-row justify-between pb-4 items-center'>
         <h2 className='text-[30px] font-bold'>Shop By Brands</h2>
+        <p className='flex-1 h-2 w-full bg-cardBorderShadow rounded-md mx-2'></p>
         <button className='border-4 border-solid  border-cardBorderShadow px-4 py-1 rounded-3xl cursor-pointer'>View All Brands</button>
       </div>
       <div className='flex flex-row justify-between'>
