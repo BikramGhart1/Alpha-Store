c9ad9377f7a172bd6428223d80d43c17ebe1a316
shawinCreates <sabinneupane31@gmail.com>
2025-03-18 15:34:45 +0545

    managed shop by categories

--- src/components/ShopeByCategories.jsx
+++ src/components/ShopeByCategories.jsx
@@ -1,53 +1,38 @@
-import React from 'react'
+import React from "react";
 
-export default function ShopeByCategories() {
+const categories = [
+  { name: "Smartphones", image: "/images/smartphones.png" },
+  { name: "Smartwatch", image: "/images/smartwatch.png" },
+  { name: "Earbuds", image: "/images/earbuds.png" },
+  { name: "Speakers", image: "/images/speakers.png" },
+  { name: "Drones", image: "/images/drones.png" },
+  { name: "Laptops", image: "/images/laptops.png" },
+];
+
+export default function ShopByCategories() {
   return (
-    <section className='section borderBottom '>
-      <p className='sectionHeader'>Shop By Categories</p>
-      <div className='flex flex-wrap gap-x-5 gap-y-10'>
-        <div className='bg-navbar flex flex-row text-white p-2 gap-x-4'>
-          <div className=''>
-            <p className='text-2xl'>Smartphones</p>
-            <p>VIEW ALL</p>
+    <section className="section px-5">
+      <p className="text-3xl font-semibold text-black mb-6">Shop By Categories</p>
+      <div className="grid grid-cols-3 gap-5">
+        {categories.map((category, index) => (
+          <div
+            key={index}
+            className="bg-[#004080] flex justify-between items-center p-5 rounded-lg text-white"
+          >
+            <div>
+              <p className="text-2xl font-medium">{category.name}</p>
+              <p className="text-sm cursor-pointer opacity-75 hover:opacity-100">VIEW ALL</p>
+            </div>
+            <div className="pl-5">
+              <img
+                src={category.image}
+                alt={category.name}
+                className="object-cover rounded-lg"
+              />
+            </div>
           </div>
-          <img src="/images/Rectangle 66.png" alt="categories" />
-        </div>
-        <div className='bg-navbar flex flex-row text-white p-2 gap-x-4'>
-          <div className=''>
-            <p className='text-2xl'>Smartphones</p>
-            <p>VIEW ALL</p>
-          </div>
-          <img src="/images/Rectangle 68.png" alt="categories" />
-        </div>
-        <div className='bg-navbar flex flex-row text-white p-2 gap-x-4'>
-          <div className=''>
-            <p className='text-2xl'>Smartphones</p>
-            <p>VIEW ALL</p>
-          </div>
-          <img src="/images/Rectangle 68.png" alt="categories" />
-        </div>
-        <div className='bg-navbar flex flex-row text-white p-2 gap-x-4'>
-          <div className=''>
-            <p className='text-2xl'>Smartphones</p>
-            <p>VIEW ALL</p>
-          </div>
-          <img src="/images/Rectangle 68.png" alt="categories" />
-        </div>
-        <div className='bg-navbar flex flex-row text-white p-2 gap-x-4'>
-          <div className=''>
-            <p className='text-2xl'>Smartphones</p>
-            <p>VIEW ALL</p>
-          </div>
-          <img src="/images/Rectangle 68.png" alt="categories" />
-        </div>
-        <div className='bg-navbar flex flex-row text-white p-2 gap-x-4'>
-          <div className=''>
-            <p className='text-2xl'>Smartphones</p>
-            <p>VIEW ALL</p>
-          </div>
-          <img src="/images/Rectangle 66.png" alt="categories" />
-        </div>
+        ))}
       </div>
     </section>
-  )
+  );
 }
