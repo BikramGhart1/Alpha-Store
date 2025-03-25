f052e2d3cc97ff50a64e3a877a3f9e1e539a0b8c
rtimalsina947 <rtimalsina947@gmail.com>
2025-03-25 22:11:55 +0545

    Add a latest post section

---
+++ src/components/partials/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer>
+            <div>
+                <div>
+                    <div>
+
+                        <h2>Store Location</h2>
+                    </div>
+                    <div>
+                        <p>Dudhpati Bhaktapur</p>
+                        <p>info@alphastore.com</p>
+                        <p>+977 9000000000</p>
+                    </div>
+
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </footer>
+    )
+}
